17128994f76bd127942aeb292e609c14b7523002
Carlos Silveira <carloss.87@gmail.com>
2022-02-12 23:25:19 +0000

    done

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
